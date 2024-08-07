8763a70db3c1ccac32761547453219365172a425
yutchina <155829679+yutchina@users.noreply.github.com>
2024-08-07 22:11:25 +0800

    Update run.py

--- run.py
+++ run.py
@@ -5,14 +5,10 @@
 # @software :
 
 import os
-from utils.clip_dataloader import bert_data
-#from utils.weibo21_clip_dataloader import bert_data
-from model.mdfend import Trainer as MDFENDTrainer
-from model.domain import Trainer as DOMAINTrainer
-from model.domain_ple6 import Trainer as PLETrainer
-from model.pivot3 import Trainer as clip9Trainer
-#from model.single_ple import Trainer as SinglePLETrainer
-from model.clip_ITM import Trainer as SinglePLETrainer
+from utils.clip_dataloader import bert_data as weibo_data
+from utils.weibo21_clip_dataloader import bert_data as weibo21_data
+from model.MMDFND import Trainer as MMDFNDTrainer
+
 class Run():
     def __init__(self,
                  config
@@ -37,16 +33,13 @@ class Run():
         self.weight_decay = config['weight_decay']
         self.epoch = config['epoch']
         self.save_param_dir = config['save_param_dir']
-
-        self.train_path = self.root_path + 'train_origin.csv'#如果9个领域就要改成train.csv
-        self.val_path = self.root_path + 'val_origin.csv'#如果9个领域就要改成val.csv
-        self.test_path = self.root_path + 'test_origin.csv'#如果9个领域就要改成test.csv
-
-        #self.train_path = self.root_path + 'train_datasets.xlsx'#weibo21
-        #self.val_path = self.root_path + 'val_datasets.xlsx'#weibo21
-        #self.test_path = self.root_path + 'test_datasets.xlsx'#weibo21
-
-        self.category_dict = {
+        self.dataset = config['dataset']
+        if config['dataset']=="weibo":
+            self.root_path = './data/'
+            self.train_path = self.root_path + 'train_origin.csv'  # 如果9个领域就要改成train.csv
+            self.val_path = self.root_path + 'val_origin.csv'  # 如果9个领域就要改成val.csv
+            self.test_path = self.root_path + 'test_origin.csv'  # 如果9个领域就要改成test.csv
+            self.category_dict = {
                 "经济": 0,
                 "健康": 1,
                 "军事": 2,
@@ -56,7 +49,24 @@ class Run():
                 "教育": 6,
                 "娱乐": 7,
                 "社会": 8
-        }
+            }
+        if config['dataset']=="weibo21":
+            self.root_path = './Weibo_21/'
+            self.train_path = self.root_path + 'train_datasets.xlsx'#weibo21
+            self.val_path = self.root_path + 'val_datasets.xlsx'#weibo21
+            self.test_path = self.root_path + 'test_datasets.xlsx'#weibo21
+            self.category_dict = {
+                "科技": 0,
+                "军事": 1,
+                "教育考试": 2,
+                "灾难事故": 3,
+                "政治": 4,
+                "医药健康": 5,
+                "财经商业": 6,
+                "文体娱乐": 7,
+                "社会生活": 8
+            }
+
         #self.category_dict = {
         #       "经济": 0,
         #       "健康": 1,
@@ -78,23 +88,28 @@ class Run():
         #    "文体娱乐": 7,
         #    "社会生活": 8
         #}
-    def get_dataloader(self):
+    def get_dataloader(self,dataset):
         if self.emb_type == 'bert':
-           loader = bert_data(max_len=self.max_len, batch_size=self.batchsize, vocab_file=self.vocab_file,
+            if dataset =="weibo":
+                loader = weibo_data(max_len=self.max_len, batch_size=self.batchsize, vocab_file=self.vocab_file,
                               category_dict=self.category_dict, num_workers=self.num_workers)
-
+            if dataset =="weibo21":
+                loader = weibo21_data(max_len=self.max_len, batch_size=self.batchsize, vocab_file=self.vocab_file,
+                              category_dict=self.category_dict, num_workers=self.num_workers)
         #train_loader = loader.load_data(self.train_path,'data/train_cut_loader.pkl',True)
         #val_loader = loader.load_data(self.val_path,'data/val_cut_loader.pkl',False)
         #test_loader = loader.load_data(self.test_path,'data/test_cut_loader.pkl',False)
         # 如果9个领域就要改成train_loader.csv这种
         #clip_weibo
-        train_loader = loader.load_data(self.train_path,'data/train_loader.pkl','data/train_clip_loader.pkl',True)
-        val_loader = loader.load_data(self.val_path,'data/val_loader.pkl','data/val_clip_loader.pkl',False)
-        test_loader = loader.load_data(self.test_path,'data/test_loader.pkl','data/test_clip_loader.pkl',False)
+        if dataset =="weibo":
+            train_loader = loader.load_data(self.train_path,'data/train_loader.pkl','data/train_clip_loader.pkl',True)
+            val_loader = loader.load_data(self.val_path,'data/val_loader.pkl','data/val_clip_loader.pkl',False)
+            test_loader = loader.load_data(self.test_path,'data/test_loader.pkl','data/test_clip_loader.pkl',False)
         # clip_weibo21
-        #train_loader = loader.load_data(self.train_path, 'Weibo_21/train_loader.pkl', 'Weibo_21/train_clip_loader.pkl', True)
-        #val_loader = loader.load_data(self.val_path, 'Weibo_21/val_loader.pkl', 'Weibo_21/val_clip_loader.pkl', False)
-        #test_loader = loader.load_data(self.test_path, 'Weibo_21/test_loader.pkl', 'Weibo_21/test_clip_loader.pkl', False)
+        if dataset =="weibo21":
+            train_loader = loader.load_data(self.train_path, 'Weibo_21/train_loader.pkl', 'Weibo_21/train_clip_loader.pkl', True)
+            val_loader = loader.load_data(self.val_path, 'Weibo_21/val_loader.pkl', 'Weibo_21/val_clip_loader.pkl', False)
+            test_loader = loader.load_data(self.test_path, 'Weibo_21/test_loader.pkl', 'Weibo_21/test_clip_loader.pkl', False)
         return train_loader, val_loader, test_loader
 
     def config2dict(self):
@@ -104,48 +119,13 @@ class Run():
         return config_dict
 
     def main(self):
-        train_loader, val_loader, test_loader = self.get_dataloader()
+        train_loader, val_loader, test_loader = self.get_dataloader(self.dataset)
 
 
-        if self.model_name == 'mdfend':
-            trainer = MDFENDTrainer(emb_dim=self.emb_dim, mlp_dims=self.mlp_dims, bert=self.bert,
+        if self.model_name == 'mmdfnd':
+            trainer = MMDFNDTrainer(emb_dim=self.emb_dim, mlp_dims=self.mlp_dims, bert=self.bert,
                                     use_cuda=self.use_cuda, lr=self.lr, train_loader=train_loader, dropout=self.dropout,
                                     weight_decay=self.weight_decay, val_loader=val_loader, test_loader=test_loader,
                                     category_dict=self.category_dict, early_stop=self.early_stop, epoches=self.epoch,
                                     save_param_dir=os.path.join(self.save_param_dir, self.model_name))
-
-
-        if self.model_name == 'domain':
-            trainer = DOMAINTrainer(emb_dim=self.emb_dim, mlp_dims=self.mlp_dims, bert=self.bert,
-                                    use_cuda=self.use_cuda, lr=self.lr, train_loader=train_loader, dropout=self.dropout,
-                                    weight_decay=self.weight_decay, val_loader=val_loader, test_loader=test_loader,
-                                    category_dict=self.category_dict, early_stop=self.early_stop, epoches=self.epoch,
-                                    save_param_dir=os.path.join(self.save_param_dir, self.model_name))
-
-
-        if self.model_name == 'domain_ple6':
-            trainer = PLETrainer(emb_dim=self.emb_dim, mlp_dims=self.mlp_dims, bert=self.bert,
-                                    use_cuda=self.use_cuda, lr=self.lr, train_loader=train_loader, dropout=self.dropout,
-                                    weight_decay=self.weight_decay, val_loader=val_loader, test_loader=test_loader,
-                                    category_dict=self.category_dict, early_stop=self.early_stop, epoches=self.epoch,
-                                    save_param_dir=os.path.join(self.save_param_dir, self.model_name))
-        if self.model_name == 'single_ple':
-            trainer = SinglePLETrainer(emb_dim=self.emb_dim, mlp_dims=self.mlp_dims, bert=self.bert,
-                                    use_cuda=self.use_cuda, lr=self.lr, train_loader=train_loader, dropout=self.dropout,
-                                    weight_decay=self.weight_decay, val_loader=val_loader, test_loader=test_loader,
-                                    category_dict=self.category_dict, early_stop=self.early_stop, epoches=self.epoch,
-                                    save_param_dir=os.path.join(self.save_param_dir, self.model_name))
-        if self.model_name == 'clip_domain':
-            trainer = SinglePLETrainer(emb_dim=self.emb_dim, mlp_dims=self.mlp_dims, bert=self.bert,
-                                   use_cuda=self.use_cuda, lr=self.lr, train_loader=train_loader, dropout=self.dropout,
-                                   weight_decay=self.weight_decay, val_loader=val_loader, test_loader=test_loader,
-                                   category_dict=self.category_dict, early_stop=self.early_stop, epoches=self.epoch,
-                                   save_param_dir=os.path.join(self.save_param_dir, self.model_name))
-        if self.model_name == 'pivot3':
-            trainer = clip9Trainer(emb_dim=self.emb_dim, mlp_dims=self.mlp_dims, bert=self.bert,
-                                   use_cuda=self.use_cuda, lr=self.lr, train_loader=train_loader, dropout=self.dropout,
-                                   weight_decay=self.weight_decay, val_loader=val_loader, test_loader=test_loader,
-                                   category_dict=self.category_dict, early_stop=self.early_stop, epoches=self.epoch,
-                                   save_param_dir=os.path.join(self.save_param_dir, self.model_name))
-
         trainer.train()
